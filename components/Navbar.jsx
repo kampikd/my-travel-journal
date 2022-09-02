607c519fc339b0a76c42ca339271cb755c0537d2
Damian Kampik <damian.kampik@u2i.com>
2022-09-02 10:35:38 +0200

    Add Navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <i className="fa-solid fa-earth-europe"></i>
+            <p>my travel journal.</p>
+        </nav>
+    )
+}
